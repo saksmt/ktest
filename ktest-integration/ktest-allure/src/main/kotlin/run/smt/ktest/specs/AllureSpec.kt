caac24a06c2f8af058351941f2c3d31812a7f52c
Kirill Saksin <k.saksin@tinkoff.ru>
2018-08-11 02:19:37 +0300

    Removed ladderness from AllureSpec

--- ktest-integration/ktest-allure/src/main/kotlin/run/smt/ktest/specs/AllureSpec.kt
+++ ktest-integration/ktest-allure/src/main/kotlin/run/smt/ktest/specs/AllureSpec.kt
@@ -11,6 +11,37 @@ abstract class AllureSpec(init: AllureSpec.() -> Unit) : BaseSpec() {
         this.init()
     }
 
+    inner class AllurePath internal constructor(val epic: String? = null,
+                                                    val feature: String? = null,
+                                                    val story: String? = null,
+                                                    val case: String? = null) {
+        operator fun invoke(metaInfo: MetaInfoDSL = {}, testBody: () -> Unit) {
+            fun genSuite(name: String?, suiteDef: (String, () -> Unit) -> Unit, body: () -> Unit) {
+                name?.let { suiteDef(it, body) } ?: body()
+            }
+
+            genSuite(epic, this@AllureSpec::epic) {
+                genSuite(feature, this@AllureSpec::feature) {
+                    genSuite(story, this@AllureSpec::story) {
+                        case?.let { case(it, metaInfo, testBody) } ?: case("test", metaInfo, testBody)
+                    }
+                }
+            }
+        }
+
+        operator fun plus(other: AllurePath) = AllurePath(
+            epic = other.epic ?: epic,
+            feature = other.feature ?: feature,
+            story = other.story ?: story,
+            case = other.case ?: case
+        )
+    }
+
+    fun epic(name: String): AllurePath = AllurePath(epic = name)
+    fun feature(name: String): AllurePath = AllurePath(story = name)
+    fun story(name: String): AllurePath = AllurePath(story = name)
+    fun case(name: String): AllurePath = AllurePath(case = name)
+
     fun <T> epic(name: String, body: () -> T) = epic(name, {}, body)
     fun <T> epic(name: String, metaInfo: MetaInfoDSL, body: () -> T) = SpecBuilder.suite(
         "Epic: $name",
